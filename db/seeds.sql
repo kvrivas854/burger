420c9119f5a160b6cbcbf8d65c9888327c441889
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-07-30 15:27:34 -0400

    modified

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burgerName) VALUES ('big mac');
 INSERT INTO burgers (burgerName) VALUES ('whopper');
-INSERT INTO burgers (burgerName, devoured) VALUES ('bacon cheeseburger', true);
-INSERT INTO burgers (burgerName, devoured) VALUES ('quarter pounder', true);
-INSERT INTO burgers (burgerName, devoured) VALUES ('turkey burger', true);
+INSERT INTO burgers (burgerName) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burgerName) VALUES ('quarter pounder');
+INSERT INTO burgers (burgerName) VALUES ('turkey burger');
 INSERT INTO burgers (burgerName) VALUES ('veggie burger');
